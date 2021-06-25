d5b6677021425dde85a247a043b5c184769c7b3c
Sikal Sikal <sikal.sikal.ss@gmail.com>
2021-06-25 16:33:31 +0100

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '05169abfa9b9712d467073e883ec8217'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '95ba9dc5-0d6e-47b2-a836-0d66a2ca1656'
+};
